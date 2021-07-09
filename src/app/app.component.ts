19101082edc2d75ad1453369cf717fe7adeaa01e
Julian Samuel <julian@juliansamuel.co.uk>
2021-07-09 16:40:04 +0100

    Convert to Universal and add basic HttpClient test to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'universal-demo';
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.http.get<any>('https://my-json-server.typicode.com/typicode/demo/comments').subscribe(items => {
+      console.log('items', items);
+    }
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
